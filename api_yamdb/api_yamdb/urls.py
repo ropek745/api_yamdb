24c286a897d554b5dc6caddbdab4e3187cea4ac0
Roman Pekarev <hawkdanilow@yandex.ru>
2022-07-06 16:32:22 +0300

    Вторая итерация

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -6,7 +6,6 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf.urls import url
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
@@ -17,11 +16,10 @@ urlpatterns = [
     ),
 ]
 
-
 schema_view = get_schema_view(
     openapi.Info(
-       title="Yamdb API",
-       default_version='v1',
+        title="Yamdb API",
+        default_version='v1',
         description="Документация для приложения reviews проекта Yamdb",
         # terms_of_service="URL страницы с пользовательским соглашением",
         # contact=openapi.Contact(email="admin@yamdb.ru"),
@@ -31,12 +29,11 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
-
 urlpatterns += [
-   url(r'^swagger(?P<format>\.json|\.yaml)$',
-       schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
-       name='schema-swagger-ui'),
-   url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-       name='schema-redoc'),
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'),
 ]
