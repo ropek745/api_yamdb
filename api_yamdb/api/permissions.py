e9ae8a597da9d8541b70ab4af53a2381e939e9e0
Roman Pekarev <hawkdanilow@yandex.ru>
2022-07-08 18:48:50 +0300

    Исправлены ошибки второго ревью

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -14,10 +14,11 @@ class AdminOrReadOnly(BasePermission):
 
 class AuthorOrStaffOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return (request.method in SAFE_METHODS
-                or request.user == obj.author
-                or request.user.is_admin
-                or request.user.is_moderator)
+        return (
+            request.method in SAFE_METHODS
+            or request.user == obj.author
+            or request.user.is_moderator
+            )
 
 
 class AdminOnly(BasePermission):
